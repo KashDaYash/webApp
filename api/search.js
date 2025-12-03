facd772e2325aa66aa7e8c292fa4ddefa0cb5c8c
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-12-04 01:44:22 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -3,10 +3,23 @@ const connectDB = require('./lib/db');
 const { User } = require('./lib/models');
 
 module.exports = async (req, res) => {
-  await connectDB();
-  const { query } = req.query; // Telegram username query
-  const users = await User.find({ 
-    username: { $regex: query, $options: 'i' } 
-  }).limit(10);
-  res.json(users);
+  try {
+    await connectDB();
+    const { query } = req.query;
+
+    if (!query) return res.json([]);
+
+    // Search logic: Username YA First Name me dhundo
+    const users = await User.find({
+      $or: [
+        { username: { $regex: query, $options: 'i' } },   // Case insensitive username
+        { first_name: { $regex: query, $options: 'i' } }  // Case insensitive name
+      ]
+    }).limit(20);
+
+    res.json(users);
+  } catch (error) {
+    console.error("Search Error:", error);
+    res.status(500).json([]);
+  }
 };
