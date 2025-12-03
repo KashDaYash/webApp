c8b2e226df8dde1db355b79c5d4bf5532bfb0df1
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-12-04 01:47:42 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -3,23 +3,23 @@ const connectDB = require('./lib/db');
 const { User } = require('./lib/models');
 
 module.exports = async (req, res) => {
+  await connectDB();
+  const { query } = req.query;
+
+  // Agar user ne kuch nahi likha, to search mat karo
+  if (!query || query.length < 1) return res.json([]);
+
   try {
-    await connectDB();
-    const { query } = req.query;
-
-    if (!query) return res.json([]);
-
-    // Search logic: Username YA First Name me dhundo
+    // "regex" use kar rahe hain pattern matching ke liye (Case Insensitive)
     const users = await User.find({
       $or: [
-        { username: { $regex: query, $options: 'i' } },   // Case insensitive username
-        { first_name: { $regex: query, $options: 'i' } }  // Case insensitive name
+        { username: { $regex: "^" + query, $options: 'i' } }, // Username starts with query
+        { first_name: { $regex: "^" + query, $options: 'i' } } // Name starts with query
       ]
-    }).limit(20);
+    }).limit(10).select('tg_id username first_name photo_url'); // Sirf zaroori data bhejo
 
     res.json(users);
-  } catch (error) {
-    console.error("Search Error:", error);
+  } catch (e) {
     res.status(500).json([]);
   }
 };
