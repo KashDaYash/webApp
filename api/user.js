6d0dfa90e0856e90efa267511766b33537cd8bbe
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-12-03 09:02:29 +0530

    Implement user API with GET and POST methods

---
+++ api/user.js
@@ -0,0 +1,11 @@
+let cache = {};
+
+export default function handler(req, res){
+  if(req.method === "POST"){
+    cache = req.body;
+    return res.status(200).json({ok:true});
+  }
+  if(req.method === "GET"){
+    return res.status(200).json(cache);
+  }
+}
