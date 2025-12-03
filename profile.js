44c629013e4270bc6305e75b59772e49d60e5f09
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-12-03 21:22:32 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -79,7 +79,7 @@ toggle.addEventListener("click", () => {
 // --- USER DATA (Clean One-Liner Logic) ---
 // Yahan humne wahi logic use kiya jo aapne bola 👇
 const user = tg.initDataUnsafe?.user || {};
-
+alert(user);
 
 // Language detect logic
 let langCode = localStorage.getItem("languageCode");
