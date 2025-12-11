5ef11083985ccd29274cd30364df4332292c3c05
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-12-11 20:54:25 +0530

    Update models.js

--- api/lib/models.js
+++ api/lib/models.js
@@ -5,15 +5,16 @@ const UserSchema = new mongoose.Schema({
   username: String,
   first_name: String,
   photo_url: String,
+  is_verified: { type: Boolean, default: false }, // Blue Tick
   last_seen: { type: Date, default: Date.now },
-  last_typed: { type: Date, default: Date.now } // NEW: Typing status ke liye
+  last_typed: { type: Date } // For Typing Indicator
 });
 
 const MessageSchema = new mongoose.Schema({
   sender_id: Number,
   receiver_id: Number,
   text: String,
-  is_read: { type: Boolean, default: false }, 
+  is_read: { type: Boolean, default: false },
   timestamp: { type: Date, default: Date.now }
 });
 
